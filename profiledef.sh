44fdd34017acb6eaf1fcc4235f535f7674aecb6d
michael <davletkildeev.michael@gmail.com>
2026-01-18 12:48:31 +0100

    copyscripts fix

--- profiledef.sh
+++ profiledef.sh
@@ -26,4 +26,5 @@ file_permissions=(
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/bin/installgrub"]="0:0:755"
+  ["/usr/bin/copyscripts"]="0:0:755"
 )
